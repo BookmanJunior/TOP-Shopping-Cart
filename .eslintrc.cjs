e79222feef494786d4874186bb4e97bd573d01fc
Azam Gulov <gulovazam@gmail.com>
2023-09-26 20:44:34 +0500

    add airbnb extension

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,6 +6,8 @@ module.exports = {
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
     "prettier",
+    "airbnb",
+    "airbnb-typescript",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
