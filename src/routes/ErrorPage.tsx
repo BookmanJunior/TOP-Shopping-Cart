39360d82e1118b8215f272ee166c0aa9daff10ae
Azam Gulov <gulovazam@gmail.com>
2023-10-09 15:17:05 +0500

    Store.tsx

--- src/routes/ErrorPage.tsx
+++ src/routes/ErrorPage.tsx
@@ -1,3 +1,10 @@
 import { Link } from "react-router-dom";
 
-export default function ErrorPage() {}
+export default function ErrorPage() {
+  return (
+    <div className="error-page">
+      OOPS, Something went wrong!
+      <Link to="..">Go Back</Link>
+    </div>
+  );
+}
--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -1,3 +1,58 @@
+import { useState } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "../App";
+import Store from "./Store";
+import Cart from "./Cart";
+import ErrorPage from "./ErrorPage";
 
-export default function Router() {}
+export default function Router() {
+  const [cartItems, setCartItems] = useState<{}[]>([]);
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "store",
+          element: <Store />,
+        },
+        {
+          path: "cart",
+          element: <Cart />,
+        },
+      ],
+    },
+    // {
+    //   path: "store",
+    //   element: <Store />,
+    // },
+    // {
+    //   path: "store/:category",
+    //   element: <Products name="electronics" />,
+    // },
+    // {
+    //   path: "store/electronics",
+    //   element: <Products name="electronics" />,
+    // },
+    // {
+    //   path: "store/jewelery",
+    //   element: <Products name="jewelery" />,
+    // },
+    // {
+    //   path: "store/men's clothing",
+    //   element: <Products name="men's clothing" />,
+    // },
+    // {
+    //   path: "store/women's clothing",
+    //   element: <Products name="women's clothing" />,
+    // },
+    // {
+    //   path: "cart",
+    //   element: <Cart />,
+    // },
+  ]);
+
+  return <RouterProvider router={router} />;
+}
