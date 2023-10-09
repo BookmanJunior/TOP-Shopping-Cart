96f55f62675139bdc23424ce810b45051365ad03
Azam Gulov <gulovazam@gmail.com>
2023-10-09 15:08:52 +0500

    Add nav elements

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,1 +1,22 @@
-export default function Nav() {}
+import { Link } from "react-router-dom";
+import "../styles/nav.css";
+
+export default function Nav() {
+  return (
+    <nav id="nav">
+      <div className="nav-wrapper">
+        <ul className="nav-links">
+          <li className="nav-link">
+            <Link to="/">Home</Link>
+          </li>
+          <li className="nav-link">
+            <Link to="../store">Store</Link>
+          </li>
+          <li className="nav-link">
+            <Link to="cart">Cart</Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
