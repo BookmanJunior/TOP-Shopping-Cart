14a765867853112d431282350a5587285154b51f
Azam Gulov <gulovazam@gmail.com>
2023-10-10 21:48:11 +0500

    Lift up products state
    add cartItems state
    add function to decrement and increment item quantity
    add default page

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,10 +3,38 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "../App";
 import Store from "./Store";
 import Cart from "./Cart";
+import Header from "../components/Header";
 import ErrorPage from "./ErrorPage";
 
 export default function Router() {
-  const [cartItems, setCartItems] = useState<{}[]>([]);
+  const [products, setProducts] = useState<ProductData[]>([] as ProductData[]);
+  const [cartItems, setCartItems] = useState<CartItemProps[]>([]);
+
+  function handleCartAdd(item: CartItemProps) {
+    setCartItems([...cartItems, { ...item, quantity: 1 }]);
+  }
+
+  function incrementProductQuantity(item: number) {
+    setCartItems(
+      cartItems.map((cartItem) => {
+        if (cartItem.id === item) {
+          return { ...cartItem, quantity: cartItem.quantity + 1 };
+        }
+        return cartItem;
+      })
+    );
+  }
+
+  function decrementProductQuantity(item: number) {
+    setCartItems(
+      cartItems.map((cartItem) => {
+        if (cartItem.id === item) {
+          return { ...cartItem, quantity: cartItem.quantity - 1 };
+        }
+        return cartItem;
+      })
+    );
+  }
 
   const router = createBrowserRouter([
     {
@@ -15,43 +43,28 @@ export default function Router() {
       errorElement: <ErrorPage />,
       children: [
         {
+          index: true,
+          element: <Header />,
+        },
+        {
           path: "store",
-          element: <Store />,
+          element: (
+            <Store
+              products={products}
+              setProducts={setProducts}
+              cartItems={cartItems}
+              handleCartAdd={handleCartAdd}
+              handleIncrementQuantity={incrementProductQuantity}
+              handleDecrementQuantity={decrementProductQuantity}
+            />
+          ),
         },
         {
           path: "cart",
-          element: <Cart />,
+          element: <Cart cartItems={cartItems} />,
         },
       ],
     },
-    // {
-    //   path: "store",
-    //   element: <Store />,
-    // },
-    // {
-    //   path: "store/:category",
-    //   element: <Products name="electronics" />,
-    // },
-    // {
-    //   path: "store/electronics",
-    //   element: <Products name="electronics" />,
-    // },
-    // {
-    //   path: "store/jewelery",
-    //   element: <Products name="jewelery" />,
-    // },
-    // {
-    //   path: "store/men's clothing",
-    //   element: <Products name="men's clothing" />,
-    // },
-    // {
-    //   path: "store/women's clothing",
-    //   element: <Products name="women's clothing" />,
-    // },
-    // {
-    //   path: "cart",
-    //   element: <Cart />,
-    // },
   ]);
 
   return <RouterProvider router={router} />;
