9f26b9c9d0303923d0be32e53e3d998b33504d9b
Azam Gulov <gulovazam@gmail.com>
2023-10-10 21:49:08 +0500

    add carItems prop
    calculate total price of items
    add conditional display

--- src/routes/Cart.tsx
+++ src/routes/Cart.tsx
@@ -1,10 +1,29 @@
 import { Link } from "react-router-dom";
 
-export default function Cart() {
+type CartProps = {
+  cartItems: CartItemProps[];
+};
+
+export default function Cart({ cartItems }: CartProps) {
+  const total: number = cartItems.reduce((prev, curr): number => {
+    return (prev + Math.round(parseFloat(curr.price))) * curr.quantity;
+  }, 0);
+
   return (
     <div>
-      Your Cart is Empty.
-      <Link to="../store">Browse Shop</Link>
+      {!cartItems.length ? (
+        <div>
+          Your Cart is Empty.
+          <Link to="../store">Browse Shop</Link>
+        </div>
+      ) : (
+        <div>
+          {cartItems.map((item) => (
+            <div key={item.id}>{item.title}</div>
+          ))}
+          {`Total: $${total}`}
+        </div>
+      )}
     </div>
   );
 }
