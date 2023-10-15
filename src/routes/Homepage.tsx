c90fc8cbc8b9e3a73aa3564d1db06f248cf29329
Gulov Azam <gulovazam@gmail.com>
2023-10-15 16:19:20 +0500

    Add categories
    add category loader
    import images

--- src/routes/Homepage.tsx
+++ src/routes/Homepage.tsx
@@ -1,10 +1,40 @@
 import Header from "../components/Header";
+import ElectronicsImg from "../assets/electronics.jpeg";
+import JewelryImg from "../assets/jewelry.avif";
+import MClothingImg from "../assets/men's clothing.jpg";
+import WClothingImg from "../assets/women's clothing.avif";
+import FeaturedCard from "../components/FeaturedCard";
 
-export default function Homepage() {
+export function Homepage() {
   return (
     <>
       <Header />
-      <section className="featured-categories"></section>
+      <section className="featured-categories-wrapper">
+        <p className="section-title">Categories</p>
+        <div className="featured-categories">
+          <FeaturedCard title={"electronics"} imageElement={ElectronicsImg} />
+          <FeaturedCard title={"jewelery"} imageElement={JewelryImg} />
+          <FeaturedCard title={"men's clothing"} imageElement={MClothingImg} />
+          <FeaturedCard
+            title={"women's clothing"}
+            imageElement={WClothingImg}
+          />
+        </div>
+      </section>
     </>
   );
+}
+
+type CategoryLoaderProps = {
+  params: { name: string };
+};
+
+export async function CategoryLoader({ params }: CategoryLoaderProps) {
+  const res = await fetch(
+    `https://fakestoreapi.com/products/category/${params.name}`
+  );
+
+  if (res.ok) return res.json();
+
+  throw new Error();
 }
