86b05cc1e53234cc532baf8aacb413c5b39943c7
Azam Gulov <gulovazam@gmail.com>
2023-10-17 18:02:48 +0500

    Import hero images
    add picture tag

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,22 +1,29 @@
 import HeroImage from "../assets/image-hero.png";
+import HeroImageTablet from "../assets/image-header-tablet.jpg";
+import HeroImagePhone from "../assets/image-header-phone.jpg";
 
 export default function Header() {
   return (
     <header className="homepage-header">
-      <div className="product-info">
-        <span className="product-new">New Product</span>
-        <p className="product-name">XX99 MARK II HEADPHONES</p>
-        <p className="product-desc">
-          Experience natural, lifelike audio and exceptional build quality made
-          for the passionate music enthusiast.
-        </p>
-        <button>See Product</button>
+      <div className="homepage-header-wrapper">
+        <div className="product-info">
+          <span className="product-new">New Product</span>
+          <p className="product-name">XX99 MARK II HEADPHONES</p>
+          <p className="product-desc">
+            Experience natural, lifelike audio and exceptional build quality
+            made for the passionate music enthusiast.
+          </p>
+        </div>
+        <picture>
+          <source media="(min-width: 950px)" srcSet={HeroImage} />
+          <source media="(max-width: 500px)" srcSet={HeroImagePhone} />
+          <img
+            src={HeroImageTablet}
+            alt="XX99 MARK II HEADPHONES Image"
+            className="hero-img"
+          />
+        </picture>
       </div>
-      <img
-        src={HeroImage}
-        alt="XX99 MARK II HEADPHONES Image"
-        className="hero-img"
-      />
     </header>
   );
 }
