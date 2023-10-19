e57d8e986ca1b481f9c9ef17531912d7a4b16ba0
Azam Gulov <gulovazam@gmail.com>
2023-10-19 16:17:56 +0500

    Update outlet context
    Add handleDelete function
    add context type

--- src/App.tsx
+++ src/App.tsx
@@ -7,26 +7,48 @@ type ContextType = {
   products: ProductData[];
   setProducts: (arg0: ProductData[]) => void;
   cartItems: CartItemProps[];
-  setCartItems: (arg0: CartItemProps[]) => void;
+  setCartItems: (arg0: ProductData[]) => void;
+  setIsModalOpen: (arg0: boolean) => void;
+  setActiveItem: (arg0: ProductData) => void;
 };
 
 export default function App() {
   const [products, setProducts] = useState<ProductData[]>([] as ProductData[]);
   const [cartItems, setCartItems] = useState<CartItemProps[]>([]);
+  const [activeItem, setActiveItem] = useState<ProductData | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   return (
     <>
       <Nav cartItemsLength={cartItems.length} />
-      {isModalOpen && (
-        <Modal
-          isModalOpen={isModalOpen}
-          closeModal={() => setIsModalOpen(false)}
-        />
-      )}
-      <Outlet context={{ cartItems, setCartItems, products, setProducts }} />
+      <Modal
+        isModalOpen={isModalOpen}
+        closeModal={() => setIsModalOpen(false)}
+        handleDelete={handleDelete}
+        activeItem={activeItem}
+      />
+      <Outlet
+        context={{
+          cartItems,
+          setCartItems,
+          products,
+          setProducts,
+          setIsModalOpen,
+          setActiveItem,
+        }}
+      />
     </>
   );
+
+  function handleDelete() {
+    if (activeItem) {
+      setCartItems(cartItems.filter((item) => item.id !== activeItem.id));
+      setActiveItem(null);
+    } else {
+      setCartItems([]);
+    }
+    setIsModalOpen(false);
+  }
 }
 
 export function AppContext() {
