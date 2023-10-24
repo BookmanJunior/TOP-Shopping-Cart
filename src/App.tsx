b24886ffb5affe5e4433b28c1dbcd7aab2f19a32
Azam Gulov <gulovazam@gmail.com>
2023-10-24 15:03:22 +0500

    Refactor code
    update types
    set activeItem to null on cancelation

--- src/App.tsx
+++ src/App.tsx
@@ -4,44 +4,12 @@ import Nav from "./components/Nav";
 import Modal from "./components/Modal";
 import ToastList from "./components/toast";
 
-type ContextType = {
-  products: ProductData[];
-  setProducts: (arg0: ProductData[]) => void;
-  cartItems: CartItemProps[];
-  setCartItems: (arg0: ProductData[]) => void;
-  setIsModalOpen: (arg0: boolean) => void;
-  setActiveItem: (arg0: ProductData) => void;
-  showToast: (data: ProductData, type: "add" | "remove") => void;
-};
-
-type toastProps = {
-  id: number;
-  title: string;
-  type: string;
-};
-
 export default function App() {
   const [products, setProducts] = useState<ProductData[]>([] as ProductData[]);
   const [cartItems, setCartItems] = useState<CartItemProps[]>([]);
   const [activeItem, setActiveItem] = useState<ProductData | null>(null);
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [toasts, setToasts] = useState<toastProps[]>([]);
-
-  function showToast(item: ProductData, type: "add" | "remove") {
-    const newToast = {
-      id: item.id,
-      title: item.title,
-      type,
-    };
-
-    setToasts((prevToasts) => [...prevToasts, newToast]);
-  }
-
-  function removeToast(toastId: number) {
-    setToasts((prevToasts) =>
-      prevToasts.filter((toast) => toast.id !== toastId)
-    );
-  }
+  const [toasts, setToasts] = useState<ToastDataProps[]>([]);
 
   return (
     <>
@@ -49,11 +17,14 @@ export default function App() {
       <Nav cartItemsLength={cartItems.length} />
       <Modal
         isModalOpen={isModalOpen}
-        closeModal={() => setIsModalOpen(false)}
+        closeModal={() => {
+          setIsModalOpen(false);
+          setActiveItem(null);
+        }}
         handleDelete={handleDelete}
         activeItem={activeItem}
       />
-      <ToastList data={toasts} removeToast={removeToast} />
+      <ToastList toasts={toasts} removeToast={removeToast} />
       <Outlet
         context={{
           cartItems,
@@ -68,17 +39,43 @@ export default function App() {
     </>
   );
 
+  function showToast(id: ToastId, type: ToastType) {
+    const newToast = {
+      id,
+      type,
+    };
+
+    setToasts((prevToasts) => [...prevToasts, newToast]);
+  }
+
+  function removeToast(toastId: ToastId) {
+    setToasts((prevToasts) =>
+      prevToasts.filter((toast) => toast.id !== toastId)
+    );
+  }
+
   function handleDelete() {
     if (activeItem) {
       setCartItems(cartItems.filter((item) => item.id !== activeItem.id));
-      showToast(activeItem, "remove");
+      showToast(activeItem.id, "remove");
       setActiveItem(null);
     } else {
       setCartItems([]);
+      showToast(0, "empty");
     }
     setIsModalOpen(false);
   }
 }
+
+type ContextType = {
+  products: ProductData[];
+  setProducts: (arg0: ProductData[]) => void;
+  cartItems: CartItemProps[];
+  setCartItems: (arg0: ProductData[]) => void;
+  setIsModalOpen: (arg0: boolean) => void;
+  setActiveItem: (arg0: ProductData) => void;
+  showToast: (id: ToastId, type: ToastType) => void;
+};
 
 export function AppContext() {
   return useOutletContext<ContextType>();
