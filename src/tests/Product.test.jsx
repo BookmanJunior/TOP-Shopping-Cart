bdebad205d479b13c4c0e4083b5848f3ade2370c
Azam Gulov <gulovazam@gmail.com>
2023-10-27 15:02:11 +0500

    Initial commit
    add test to render product
    add test to add product to cart
    add test to increment item quantity
    add test to decrement item quantity

---
+++ src/tests/Product.test.jsx
@@ -0,0 +1,103 @@
+import { Product } from "../components/Product";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "../App";
+
+const Router = ({ children }) => {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <App />,
+      children: [
+        {
+          index: true,
+          element: children,
+        },
+      ],
+    },
+  ]);
+
+  return <RouterProvider router={router} />;
+};
+
+describe("Product component", () => {
+  const mockData = {
+    id: 1,
+    title: "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    price: 109,
+    description:
+      "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    category: "men's clothing",
+    image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+  };
+
+  it("renders product", () => {
+    const { container } = render(
+      <Router>
+        <Product data={mockData} />
+      </Router>
+    );
+    const price = container.querySelector(".product-title");
+    expect(price.textContent).toMatch(
+      /Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops/i
+    );
+  });
+
+  it("adds product to cart", async () => {
+    render(
+      <Router>
+        <Product data={mockData} />
+      </Router>
+    );
+
+    const addToCartButton = screen.getByRole("button", { name: "Add to Cart" });
+    await userEvent.click(addToCartButton);
+
+    expect(addToCartButton).not.toBeInTheDocument();
+
+    const incrementButton = screen.getByRole("button", { name: "+" });
+    const decrementButton = screen.getByRole("button", { name: "-" });
+
+    expect(incrementButton).toBeInTheDocument();
+    expect(decrementButton).toBeInTheDocument();
+  });
+
+  it("increments product quantity", async () => {
+    render(
+      <Router>
+        <Product data={mockData} />
+      </Router>
+    );
+
+    const addToCartButton = screen.getByRole("button", { name: "Add to Cart" });
+    await userEvent.click(addToCartButton);
+
+    const incrementButton = screen.getByRole("button", { name: "+" });
+    await userEvent.click(incrementButton);
+
+    const quantity = screen.getByText(/2/i);
+
+    expect(quantity).toBeInTheDocument();
+  });
+
+  it("decrements product quantity", async () => {
+    render(
+      <Router>
+        <Product data={mockData} />
+      </Router>
+    );
+
+    const addToCartButton = screen.getByRole("button", { name: "Add to Cart" });
+    await userEvent.click(addToCartButton);
+
+    const incrementButton = screen.getByRole("button", { name: "+" });
+    await userEvent.click(incrementButton);
+
+    const decrementButton = screen.getByRole("button", { name: "-" });
+    await userEvent.click(decrementButton);
+    const quantity = screen.getByText(/1/i, { selector: "span" });
+
+    expect(quantity).toBeInTheDocument();
+  });
+});
