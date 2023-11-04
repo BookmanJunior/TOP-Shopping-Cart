9323fce8385e640bc465caa7974f4c1eee6b68bd
Azam Gulov <gulovazam@gmail.com>
2023-11-04 21:15:41 +0500

    Remove detail implementation tests

--- src/tests/Product.test.jsx
+++ src/tests/Product.test.jsx
@@ -1,7 +1,7 @@
-import { TestRouter } from "./testRouter";
+import { RenderRouteWithOutletContext } from "./RenderRouteWithOutletContext";
 import { Product } from "../components/Product";
-import { render, screen } from "@testing-library/react";
-import userEvent from "@testing-library/user-event";
+import { render } from "@testing-library/react";
+import { vi } from "vitest";
 
 describe("Product component", () => {
   const mockData = {
@@ -14,72 +14,20 @@ describe("Product component", () => {
     image: "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
   };
 
+  const mockContext = {
+    cartItems: [],
+    setCartItems: vi.fn(),
+  };
+
   it("renders product", () => {
     const { container } = render(
-      <TestRouter>
+      <RenderRouteWithOutletContext context={mockContext}>
         <Product data={mockData} />
-      </TestRouter>
+      </RenderRouteWithOutletContext>
     );
     const price = container.querySelector(".product-title");
     expect(price.textContent).toMatch(
       /Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops/i
     );
-  });
-
-  it("adds product to cart", async () => {
-    render(
-      <TestRouter>
-        <Product data={mockData} />
-      </TestRouter>
-    );
-
-    const addToCartButton = screen.getByRole("button", { name: "Add to Cart" });
-    await userEvent.click(addToCartButton);
-
-    expect(addToCartButton).not.toBeInTheDocument();
-
-    const incrementButton = screen.getByRole("button", { name: "+" });
-    const decrementButton = screen.getByRole("button", { name: "-" });
-
-    expect(incrementButton).toBeInTheDocument();
-    expect(decrementButton).toBeInTheDocument();
-  });
-
-  it("increments product quantity", async () => {
-    render(
-      <TestRouter>
-        <Product data={mockData} />
-      </TestRouter>
-    );
-
-    const addToCartButton = screen.getByRole("button", { name: "Add to Cart" });
-    await userEvent.click(addToCartButton);
-
-    const incrementButton = screen.getByRole("button", { name: "+" });
-    await userEvent.click(incrementButton);
-
-    const quantity = screen.getByText(/2/i);
-
-    expect(quantity).toBeInTheDocument();
-  });
-
-  it("decrements product quantity", async () => {
-    render(
-      <TestRouter>
-        <Product data={mockData} />
-      </TestRouter>
-    );
-
-    const addToCartButton = screen.getByRole("button", { name: "Add to Cart" });
-    await userEvent.click(addToCartButton);
-
-    const incrementButton = screen.getByRole("button", { name: "+" });
-    await userEvent.click(incrementButton);
-
-    const decrementButton = screen.getByRole("button", { name: "-" });
-    await userEvent.click(decrementButton);
-    const quantity = screen.getByText(/1/i, { selector: "span" });
-
-    expect(quantity).toBeInTheDocument();
   });
 });
