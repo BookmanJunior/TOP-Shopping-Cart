6ffab2aa5a58b75477d6b83c7b8e7c7e308ff0a3
Azam Gulov <gulovazam@gmail.com>
2024-05-03 21:28:13 +0500

    Refacor Nav
    replace css import with tailwind styles

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,5 +1,4 @@
-import { Link } from "react-router-dom";
-import "../styles/nav.css";
+import { NavLink } from "react-router-dom";
 
 type NavProps = {
   cartItemsLength: number;
@@ -7,26 +6,39 @@ type NavProps = {
 
 export default function Nav({ cartItemsLength }: NavProps) {
   return (
-    <nav id="nav">
-      <div className="nav-wrapper">
-        <ul className="nav-links">
-          <li className="nav-link">
-            <Link to="/">Home</Link>
-          </li>
-          <li className="nav-link">
-            <Link to="store">Store</Link>
-          </li>
-          <li className="nav-link">
-            <Link
-              to="cart"
-              className="cart-nav"
-              data-items={cartItemsLength > 0 ? `(${cartItemsLength})` : ""}
-            >
-              Cart
-            </Link>
-          </li>
+    <nav className="sticky top-0 z-10 ps-4 pe-4 bg-nav-color">
+      <div className="mx-[auto] w-[min(1200px,100%)] h-[4rem] border-b-[1px] border-solid border-white border-opacity-10">
+        <ul className="flex justify-center items-center gap-8 uppercase size text-[0.85rem] font-bold h-full">
+          <Link to="/">home</Link>
+          <Link to="store">store</Link>
+          <Link
+            to="cart"
+            data={cartItemsLength > 0 ? `(${cartItemsLength})` : ""}
+          >
+            cart
+          </Link>
         </ul>
       </div>
     </nav>
+  );
+}
+
+type LinkProps = {
+  to: "/" | "store" | "cart";
+  children: string;
+  data?: string | number;
+};
+
+function Link({ to, children, data = "" }: LinkProps) {
+  return (
+    <li className={`text-secondary-text-clr has-[.active]:text-accent-clr`}>
+      <NavLink
+        to={to}
+        data-items={data}
+        className={data ? "after:content-[attr(data-items)] after:ml-1" : ""}
+      >
+        {children}
+      </NavLink>
+    </li>
   );
 }
