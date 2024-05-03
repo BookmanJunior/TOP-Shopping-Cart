1ae836267a84027824e499e7b0a2a02326579e71
Azam Gulov <gulovazam@gmail.com>
2024-05-03 21:40:33 +0500

    Style component with tailwind

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,7 +1,7 @@
 export default function Spinner() {
   return (
-    <div className="loading-screen">
-      <div className="spinner"></div>
+    <div className="grid place-content-center h-screen">
+      <div className="animate-spin w-[50px] h-[50px] border-[5px] rounded-[50%] border-gray-600 border-t-accent-clr bg-transparent"></div>
     </div>
   );
 }
