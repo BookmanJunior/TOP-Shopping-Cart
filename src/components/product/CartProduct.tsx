2aac4da181105c435467ba6da59d1acbf81a404b
Gulov Azam <gulovazam@gmail.com>
2024-05-05 02:51:59 +0500

    Clicking on product item or title redirects to product page

--- src/components/product/CartProduct.tsx
+++ src/components/product/CartProduct.tsx
@@ -1,20 +1,24 @@
 import { AppContext } from "../../App";
 import FormattedPrice from "./FormattedPrice";
 import ProductButtons from "./ProductButtons";
+import { Link } from "react-router-dom";
 
 export default function CartProduct({ data }: { data: ProductData }) {
   const { setCartItems, cartItems, showToast } = AppContext();
 
   return (
     <div className="flex max-h-[300px] max-[850px]:flex-col justify-between flex-wrap gap-y-2 first:rounded-t-[8px] last:rounded-b-[8px] bg-primary-product-bg border-b-primary-bg border-b-2 p-4">
-      <div className="flex items-center text-balance gap-4 max-[850px]:flex-col max-[850px]:text-center">
+      <Link
+        to={`/store/${data.id}`}
+        className="flex items-center text-balance gap-4 max-[850px]:flex-col max-[850px]:text-center"
+      >
         <img
           src={data.image}
           alt={data.title}
-          className="w-[100px] aspect-square object-contain"
+          className="w-[100px] aspect-square object-contain hover:scale-110 transition-[transform]"
         />
         <p className="max-w-[350px]">{data.title}</p>
-      </div>
+      </Link>
       <div className="flex items-center gap-4 max-[850px]:justify-center">
         <FormattedPrice price={data.price} />
         <ProductButtons data={data} />
