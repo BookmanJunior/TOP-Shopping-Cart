1e6fc5db557f6950ecc60bfaefeb5d157f2e487e
Azam Gulov <gulovazam@gmail.com>
2024-05-04 20:32:37 +0500

    Change height

--- src/components/Spinner.tsx
+++ src/components/Spinner.tsx
@@ -1,6 +1,6 @@
 export default function Spinner() {
   return (
-    <div className="grid place-content-center h-screen">
+    <div className="grid place-content-center h-view-height">
       <div className="animate-spin w-[50px] h-[50px] border-[5px] rounded-[50%] border-gray-600 border-t-accent-clr bg-transparent"></div>
     </div>
   );
