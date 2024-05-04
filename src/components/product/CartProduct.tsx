0c1998bbe0e16bc9018c93f89b23b59b4d9c51c6
Gulov Azam <gulovazam@gmail.com>
2024-05-05 02:40:36 +0500

    Updated styling

--- src/components/product/CartProduct.tsx
+++ src/components/product/CartProduct.tsx
@@ -6,19 +6,22 @@ export default function CartProduct({ data }: { data: ProductData }) {
   const { setCartItems, cartItems, showToast } = AppContext();
 
   return (
-    <div className="flex max-h-[300px] justify-between flex-wrap bg-primary-product-bg border-b-primary-bg border-b-2 p-4">
-      <div className="flex items-center">
+    <div className="flex max-h-[300px] max-[850px]:flex-col justify-between flex-wrap gap-y-2 first:rounded-t-[8px] last:rounded-b-[8px] bg-primary-product-bg border-b-primary-bg border-b-2 p-4">
+      <div className="flex items-center text-balance gap-4 max-[850px]:flex-col max-[850px]:text-center">
         <img
           src={data.image}
           alt={data.title}
           className="w-[100px] aspect-square object-contain"
         />
-        <p className="product-title">{data.title}</p>
+        <p className="max-w-[350px]">{data.title}</p>
       </div>
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-4 max-[850px]:justify-center">
         <FormattedPrice price={data.price} />
         <ProductButtons data={data} />
-        <button className="remove-cart-item-btn" onClick={handleEmptyCart}>
+        <button
+          className="bg-primary-bg p-2 hover:outline hover:outline-2 hover:outline-primary-bg hover:outline-offset-2"
+          onClick={handleEmptyCart}
+        >
           Remove Item
         </button>
       </div>
