5ff12672f6256d3c40739ce3b7b16eff8e2c0fb2
Azam Gulov <gulovazam@gmail.com>
2024-05-04 21:10:48 +0500

    style component with tailwind
    remove css import

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef } from "react";
-import "../styles/modal.css";
 
 type ModalProps = {
   isModalOpen: boolean;
@@ -26,21 +25,28 @@ export default function Modal({
 
   return (
     isModalOpen && (
-      <dialog ref={modalRef}>
+      <dialog ref={modalRef} className="p-4 rounded-[8px] max-w-[500px]">
         {activeItem ? (
-          <p>
+          <p className="text-center">
             Are you sure you want to remove{" "}
-            <span className="modal-item-title">{activeItem.title}</span> from
-            cart?
+            <span className="font-bold">{activeItem.title}</span> from cart?
           </p>
         ) : (
-          <p className="empty-cart-warning">
-            Are you sure you want to empty cart?
-          </p>
+          <p className=" text-[1.1rem]">Are you sure you want to empty cart?</p>
         )}
-        <div className="modal-button-wrapper">
-          <button onClick={handleDelete}>Yes</button>
-          <button onClick={closeModal}>No</button>
+        <div className="text-center p-[1rem]">
+          <button
+            className="py-[0.5rem] px-[2rem] bg-accent-clr text-secondary-text-clr mr-[1rem] hover:outline-2 hover:outline-accent-clr hover:outline-offset-2 hover:outline"
+            onClick={handleDelete}
+          >
+            Yes
+          </button>
+          <button
+            className="py-[0.5rem] px-[2rem] bg-primary-bg hover:outline-2 hover:outline-primary-bg hover:outline-offset-2 hover:outline"
+            onClick={closeModal}
+          >
+            No
+          </button>
         </div>
       </dialog>
     )
