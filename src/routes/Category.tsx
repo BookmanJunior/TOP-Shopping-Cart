b89f1f69cb71a6693c91716a6292780d4c5e6e53
Gulov Azam <gulovazam@gmail.com>
2024-05-05 03:38:03 +0500

    Add styling
    remove old classes

--- src/routes/Category.tsx
+++ src/routes/Category.tsx
@@ -10,18 +10,18 @@ export function Category() {
   const categoryTitle = categoryData[0].category ?? "";
 
   return (
-    <>
-      <header className="category-header">
-        <h1 className="category-title">{categoryTitle}</h1>
+    <main>
+      <header className="text-[clamp(2rem,0.5rem+1.8vw,3rem)] text-center font-bold uppercase h-44 tracking-wider">
+        <h1>{categoryTitle}</h1>
       </header>
-      <div className="category-page-wrapper store">
-        <div className="items">
+      <div className="max-w-[1200px] mx-auto py-4">
+        <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-4">
           {categoryData?.map((item) => (
             <ProductPreview key={item.id} data={item} />
           ))}
         </div>
       </div>
-    </>
+    </main>
   );
 }
 
