3b63ac087ff715417793030d79c5ecb41301a7df
Azam Gulov <gulovazam@gmail.com>
2024-05-05 23:12:41 +0500

    Add styling

--- src/components/FeaturedCard.tsx
+++ src/components/FeaturedCard.tsx
@@ -10,11 +10,20 @@ export default function FeaturedCard({
   imageElement,
 }: FeaturedCardProps) {
   return (
-    <Link to={`category/${title}`} className="featured-category">
-      <div className="category-title-wrapper">
-        <p className="category-title">{title}</p>
+    <Link
+      to={`category/${title}`}
+      className="grid place-content-center overflow-hidden group"
+    >
+      <div className="grid place-content-center col-start-1 row-start-1 text-center bg-black bg-opacity-20">
+        <p className="text-secondary-text-clr font-bold uppercase text-[2rem] tracking-[5px]">
+          {title}
+        </p>
       </div>
-      <img src={imageElement} alt="electronics" />
+      <img
+        src={imageElement}
+        alt={title}
+        className="col-start-1 row-start-1 w-full aspect-square object-cover z-[-1] group-hover:scale-110 transition-[transform]"
+      />
     </Link>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,17 +4,21 @@ import HeroImagePhone from "../assets/image-header-phone.jpg";
 
 export default function Header() {
   return (
-    <header className="homepage-header">
-      <div className="homepage-header-wrapper">
-        <div className="product-info">
-          <span className="product-new">New Product</span>
-          <p className="product-name">XX99 MARK II HEADPHONES</p>
-          <p className="product-desc">
+    <header className="grid place-content-center bg-nav-color text-secondary-text-clr">
+      <div className="grid max-w-[1200px] h-view-height overflow-hidden">
+        <div className="col-start-1 row-start-1 max-w-[400px] p-4 self-center z-10 max-[949px]:justify-self-center max-[949px]:text-center">
+          <span className="uppercase text-dark-gray tracking-[0.625rem] text-[0.9rem]">
+            New Product
+          </span>
+          <p className="text-[clamp(2rem,10vw,3.5rem)] font-bold my-4">
+            XX99 MARK II HEADPHONES
+          </p>
+          <p className="text-dark-gray">
             Experience natural, lifelike audio and exceptional build quality
             made for the passionate music enthusiast.
           </p>
         </div>
-        <picture>
+        <picture className="col-start-1 row-start-1 [&>img]:w-full [&>img]:h-full [&>img]:object-cover">
           <source media="(min-width: 950px)" srcSet={HeroImage} />
           <source media="(max-width: 500px)" srcSet={HeroImagePhone} />
           <img
